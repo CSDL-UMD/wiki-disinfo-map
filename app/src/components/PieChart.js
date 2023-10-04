49aaac13cb7cb08c449c3aaa642833ced0077787
Daniel Pitzele <dpitzele@gmail.com>
2023-10-04 14:57:37 -0400

    removed excess comments

--- app/src/components/PieChart.js
+++ app/src/components/PieChart.js
@@ -41,17 +41,6 @@ const renderActiveShape = (props) => {
     endAngle,
     payload,
   } = props;
-  // const column = columnName(state);
-  // const sin = Math.sin(-RADIAN * midAngle);
-  // const cos = Math.cos(-RADIAN * midAngle);
-  // const sx = cx + (outerRadius + 10) * cos;
-  // const sy = cy + (outerRadius + 10) * sin;
-  // const mx = cx + (outerRadius + 30) * cos;
-  // const my = cy + (outerRadius + 30) * sin;
-  // const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  // const ey = my;
-  // const textAnchor = cos >= 0 ? 'start' : 'end';
-  // added +100 in line 57 to move text below the pie chart
   
   return (
     <g> 
@@ -80,17 +69,7 @@ const renderActiveShape = (props) => {
   );
 };
 
-/* overcomplicated demo code
-<path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-<circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-<text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`PV ${value}`}</text>
-<text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-  {`(Rate ${(percent * 100).toFixed(2)}%)`}
-</text>
-*/
-
 export default class PieChart extends Component {
-  // static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-active-shape-y93si';
 
   constructor(props) {
     super(props);
@@ -111,15 +90,8 @@ export default class PieChart extends Component {
     }
   };
 
-  // useEffect = ((_, index) => {
-  //   // make call to App filter function here
-  //   console.log("hit")
-  //   // visually highlight the selected section
-
-  // }, []);
-
   componentDidUpdate(prevProps, prevState) { 
-    // will be a problem in filter combination, should not be based on data being different
+    // TODO: will be a problem in filter combination, should not be based on data being different
     if (prevProps.data !== this.props.data) {
       // component data updated
       this.setState(() => ({
@@ -141,17 +113,7 @@ export default class PieChart extends Component {
     // this.props.rangeFilterData(this.props.column, data[this.activeIndex].name, data[this.activeIndex].name)
   };
 
-  // onSectionClick = (index) => {
-  //   // event handling for filtering data here
-  //   // this.setState({
-
-  //   // });
-  //   useEffect(index)
-  //   }
-
   render() {
-    // const pieStyle = { outline: 'none' };
-    // columnName={column};
     return (
       <>
         <Typography variant="h6" gutterBottom align="center">
