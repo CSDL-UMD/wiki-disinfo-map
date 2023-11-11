99f7b0ebfa39421e4633d41027d7d0a18d8720aa
Amrit Magesh <69167269+code890@users.noreply.github.com>
2023-11-11 12:32:27 -0500

    Continents map (#31)
    * continents-map
    
    * border
    
    * color change

--- app/src/components/Map.js
+++ app/src/components/Map.js
@@ -15,7 +15,9 @@ import { createContainsFilter } from '../utils';
 import './Map.css';
 
 // map features
-const mapFeatures = require('./mapFeatures.json');
+// const mapFeatures = require('./mapFeatures.json');
+const mapFeatures = require('./continents.json');
+
 
 const listValueCounts = (data, column) => {
   const counts = {};
@@ -25,7 +27,6 @@ const listValueCounts = (data, column) => {
       counts[key] = counts[key] ? counts[key] + 1 : 1;
     }
   }
-
   // transform to array of objects
   return Object.keys(counts).map((key) => {
     const item = { count: counts[key] };
@@ -36,7 +37,8 @@ const listValueCounts = (data, column) => {
 
 const Map = (props) => {
   const [mapCounts, setMapCounts] = useState(
-    listValueCounts(props.data, 'Country')
+    listValueCounts(props.data, 'Continent')
+    // listValueCounts(props.data, 'Country')
   );
   const [tooltipContent, setTooltipContent] = useState('');
   const [filterId, setFilterId] = useState(-1);
@@ -47,16 +49,27 @@ const Map = (props) => {
     .range(['#ffedea', '#15008c']);
     
   useEffect(() => {
-    console.log('props data updated');
-    setMapCounts(listValueCounts(props.data, 'Country'));
+    setMapCounts(listValueCounts(props.data, 'Continent'));
+    // setMapCounts(listValueCounts(props.data, 'Country'));
   }, [props.data]);
 
-  const onCountryClick = (countryName) => {
-    if (!mapCounts.find(((elem) => elem.Country === countryName))) { // checks if country has no projects
+  // const onCountryClick = (countryName) => {
+  //   if (!mapCounts.find(((elem) => elem.Country === countryName))) { // checks if country has no projects
+  //     // do nothing
+  //   } else {
+  //     props.removeFilter(filterId)
+  //     let newFilter = createContainsFilter("Country", countryName)
+  //     setFilterId(props.addFilter(newFilter))
+  //   }
+  // }
+
+  const onContinentClick = (continentName) => {
+    console.log(continentName);
+    if (!mapCounts.find(((elem) => elem.Continent === continentName))) { // checks if country has no projects
       // do nothing
     } else {
       props.removeFilter(filterId)
-      let newFilter = createContainsFilter("Country", countryName)
+      let newFilter = createContainsFilter("Continent", continentName)
       setFilterId(props.addFilter(newFilter))
     }
   }
@@ -79,23 +92,40 @@ const Map = (props) => {
               {({ geographies }) =>
                 geographies.map((geo) => {
                   const d = mapCounts.find(
-                    (s) => s.Country === geo.properties.name
+                    // (s) => s.Country === geo.properties.name
+                    (s) => s.Continent === geo.properties.continent
                   );
+
                   return (
                     <Geography
                       key={geo.rsmKey}
+                      style={{
+                        hover: { 
+                          // fill: "#04D",
+                          stroke: "#FFFFFF", // Border color on hover
+                          strokeWidth: 2, // Border width on hover
+                        },
+                        // pressed: { fill: "#02A" }
+                      }}
                       geography={geo}
                       fill={d ? colorScale(d.count) : '#ffffff'}
                       onMouseEnter={() => {
+                        console.log(d);
                         setTooltipContent(
-                          `${geo.properties.name}: ${d ? d.count : 0}`
+                          `${geo.properties.continent}: ${d ? d.count : 0}`
+                          //`${geo.properties.Country}: ${d ? d.count : 0}`
                         );
                       }}
                       onMouseLeave={() => {
                         setTooltipContent('');
                       }}
+                      // onClick={() => {
+                      //   // onCountryClick(geo.properties.name);
+                      //   onCountryClick(geo.properties.Country);
+                      // }}
                       onClick={() => {
-                        onCountryClick(geo.properties.name);
+                        // onCountryClick(geo.properties.name);
+                        onContinentClick(geo.properties.continent);
                       }}
                     />
                   );
