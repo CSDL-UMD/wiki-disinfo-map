a581ca4a634c853ff974be697b34fd4435fb5737
bluu0105 <bryant.luu0@gmail.com>
2024-01-02 15:08:18 -0500

    favicon added, appbar logo added

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -1,18 +1,35 @@
 import Typography from '@mui/material/Typography';
 
 import { AppBar, Button, Toolbar } from '@mui/material';
+import logo from "./../wikimedia_foundation_logo_white.svg";
 
 export default function ButtonAppBar({ resetData }) {
   return (
     <AppBar position="sticky">
       <Toolbar>
+        <a href="https://wikimediafoundation.org/" target="blank" rel="noopener">
+          <img src={logo} alt="" style={{ width: "175px", height: "auto" , marginRight: "0px"}}></img>
+        </a>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Wikimedia Disinformation Map
+    
         </Typography>
-        <Button color="inherit" onClick={resetData}>
+        <Button color="inherit" onClick={resetData} variant="outlined">
           Reset Filters
         </Button>
       </Toolbar>
     </AppBar>
   );
 }
+
+/*
+<AppBar position="sticky">
+  <Toolbar>
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      Wikimedia Disinformation Map
+    </Typography>
+    <Button color="inherit" onClick={resetData}>
+      Reset Filters
+    </Button>
+  </Toolbar>
+</AppBar>
+*/
--- app/src/components/ProjectDescription.js
+++ app/src/components/ProjectDescription.js
@@ -5,21 +5,25 @@ import Typography from '@mui/material/Typography';
 const ProjectDescription = () => {
   return (
     <Paper style={{ padding: 10 }}>
-      <Typography variant="h4" component="h2">
+      <Typography variant="h4" component="h2" style={{ fontFamily: "Segoe UI, Tahoma, Geneva, Verdana, sans-serif", fontWeight: "bold"}}>
         Background
       </Typography>
-      <p>
+      <p style={{fontFamily: "Segoe UI, Tahoma, Geneva, Verdana, sans-serif"}}>
         Wikipedia is a trusted source of knowledge for millions of people worldwide. 
         Edited by thousands of volunteers across more than 300 language editions, 
         the encyclopedia is part of a global movement whose goal is to build a reliable 
-        and open knowledge infrastructure for the Internet. Due to its prominence and 
+        and open knowledge infrastructure for the Internet. <br></br><br></br>
+        
+        Due to its prominence and 
         importance within cyberspace, however, Wikipedia and its sister projects are 
         constantly under threat from external actors whose role is to compromise its 
         independence and undermine its integrity as an open source of knowledge. Examples 
         include authoritarian regimes persecuting local Wikipedia editors for their role 
         in documenting conflicts like the war in Ukraine, or shady marketing firms 
         creating fake grassroots campaigns to promote companies and influence state 
-        and local decision-making processes. The Wikipedia community, however, is 
+        and local decision-making processes. <br></br><br></br>
+        
+        The Wikipedia community, however, is 
         fighting back. Over the years, a number of community projects have been started 
         to counter these threats. The goal of this project is to compile the first 
         exhaustive map of these community efforts aimed at preserving the integrity of 
