99db3b5a0a20a93af7525f4839d5fb9536eb0cc2
bluu0105 <bryant.luu0@gmail.com>
2024-02-05 10:53:27 -0500

    logos changed

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -1,15 +1,13 @@
 import Typography from '@mui/material/Typography';
 
 import { AppBar, Button, Toolbar } from '@mui/material';
-import logo from "./../wikimedia_foundation_logo_white.svg";
+import logo from "./../umd_logo_raster.png";
 
 export default function ButtonAppBar({ resetData }) {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <a href="https://wikimediafoundation.org/" target="blank" rel="noopener">
-          <img src={logo} alt="" style={{ width: "175px", height: "auto" , marginRight: "0px"}}></img>
-        </a>
+        <img src={logo} alt="" style={{ width: "50px", height: "auto" , marginRight: "0px"}}></img>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
     
         </Typography>
