e8d7b23e6ac244f728e2c0290354dff6163d3cbd
keshprad <32313895+keshprad@users.noreply.github.com>
2024-06-21 10:58:18 -0700

    update path in package.json

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -3,7 +3,6 @@ import Typography from '@mui/material/Typography';
 import { AppBar, Button, Toolbar } from '@mui/material';
 import InfoIcon from '@mui/icons-material/Info';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import logo from './../umd_logo_raster.png';
 
 export default function ButtonAppBar({ aboutRef }) {
   const submit_form = 'https://forms.gle/cC3Vfu15uVckxCV7A';
@@ -14,7 +13,7 @@ export default function ButtonAppBar({ aboutRef }) {
     <AppBar position="sticky">
       <Toolbar>
         <img
-          src={logo}
+          src={'umd_logo_raster.png'}
           alt=""
           style={{ width: '50px', height: 'auto', marginRight: '0px' }}
         ></img>
